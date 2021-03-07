57a02173b94cad716f8fc30ade75a4faae09938f
Andrea-S-Code <80191227+Andrea-S-Code@users.noreply.github.com>
2021-03-07 02:08:42 -0500

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Circle : Shape
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine("Getting area of a Circle");
+
+        }
+        public override int CalculateArea(int Height, int Width)
+        {
+            var answer = CalculateArea((int)Height, (int)Width) * Math.PI;
+            return (int)answer;
+        }
+
+    }
+}
+
---
+++ Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Rectangle : Shape
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine("Getting area of a rectangle");
+
+        }
+
+        public override int CalculateArea(int Height, int Width)
+        {
+            var answer = CalculateArea((int)Height, (int)Width);
+            return answer;
+        }
+    }
+}
