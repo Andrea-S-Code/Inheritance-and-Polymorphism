674900b63b3e19cacaf78b5615d6a65420856c33
Andrea-S-Code <80191227+Andrea-S-Code@users.noreply.github.com>
2021-03-07 02:06:34 -0500

    Add files via upload

---
+++ Shape.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Base class or Parent class.  
+class Shape
+{
+    public Shape(int height, int width, int Area)
+    {
+        Height = height;
+        Width = width;
+        Area = (int)(Height * Width);
+    }
+
+    public Shape() { }
+    public double Height;
+    public double Width;
+
+
+    public virtual void GetArea()
+    {
+        Console.WriteLine("Shape"); ;
+    }
+
+
+    public virtual void ShowDim()
+    {
+        Console.WriteLine("Width and height are " +
+        Width + " and " + Height);
+
+    }
+
+    public virtual int CalculateArea(int Height, int Width)
+    {
+        return Height * Width;
+    }
+
+}
