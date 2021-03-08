7bcd1f0f0c5265edf6fa92868cad62e1482d72c3
Andrea-S-Code <80191227+Andrea-S-Code@users.noreply.github.com>
2021-03-07 21:45:08 -0500

    Update Shape.cs

--- Shape.cs
+++ Shape.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -31,7 +31,7 @@ class Shape
     }
 
     public virtual int CalculateArea(int Height, int Width)
-    {
+    { 
         return Height * Width;
     }
 
